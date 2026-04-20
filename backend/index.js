b07149a81427ec29c37014bfab6c9eb757e02e35
monority <chenu.pro@gmail.com>
2026-04-21 00:26:20 +0200

    :bug: fixing io to null to prevent is not defined on registration

--- backend/index.js
+++ backend/index.js
@@ -173,7 +173,7 @@ const ACCESS_TOKEN_TTL = '15m';
 const REFRESH_TOKEN_TTL_MS = 7 * 24 * 60 * 60 * 1000;
 const REFRESH_TOKEN_COOKIE_NAME = process.env.REFRESH_TOKEN_COOKIE_NAME || 'skillswap_refresh';
 
-let io;
+let io = null;
 
 const signAccessToken = (user) =>
     jwt.sign(
