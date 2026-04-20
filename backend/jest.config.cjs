b43bcf0c8310921f891e92df39d295d760ffbd29
monority <chenu.pro@gmail.com>
2026-04-20 13:06:40 +0200

    refactor: simplify test setup by removing unused csrfTokens and adjusting testMatch pattern

--- backend/jest.config.cjs
+++ backend/jest.config.cjs
@@ -1,20 +1,10 @@
 module.exports = {
     testEnvironment: 'node',
-    testMatch: ['**/__tests__/**/*.test.js', '**/__tests__/**/*.test.ts'],
+    testMatch: ['**/__tests__/security/**/*.test.js'],
     setupFiles: ['./__tests__/setup.js'],
     clearMocks: true,
     restoreMocks: true,
     collectCoverage: false,
-    collectCoverageFrom: ['index.ts'],
-    coverageReporters: ['text', 'lcov'],
-    coverageThreshold: {
-        global: {
-            statements: 30,
-            branches: 20,
-            functions: 20,
-            lines: 30,
-        },
-    },
     transform: {
         '^.+\\.tsx?$': ['ts-jest', { useESM: false }],
     },
